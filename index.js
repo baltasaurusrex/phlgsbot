c4e536a26816611271748d1760782e69ffc4293c
baltiearaneta <baltiearaneta@gmail.com>
2021-12-09 14:09:54 +0800

    dec 9

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ const time_and_sales_func = (res) => {
     if (settings.update_users) updateUsers("time_and_sales", spiel);
   }
 };
-// uploadTimeAndSales("12-09-2021").then(time_and_sales_func);
+uploadTimeAndSales("12-09-2021").then(time_and_sales_func);
 
 // gets called the first time a user opens the chat
 // use this as a way to register (if not already registered)
@@ -702,7 +702,7 @@ mongoose
   .then(() => console.log("Now connected to MongoDB"))
   .catch((error) => console.log(error));
 
-fetchHistoricalPrices("311", "weekly").then((res) => console.log(res));
+// fetchHistoricalPrices("311", "weekly").then((res) => console.log(res));
 
 app.listen(port, async () => {
   // CHANGE THIS WHEN TESTING LOCALLY/ON HEROKU
