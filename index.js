80a3cffe2858a3a71c871720b89b85b433c97b48
baltiearaneta <baltiearaneta@gmail.com>
2021-12-15 10:29:08 +0800

    online = true

--- index.js
+++ index.js
@@ -706,11 +706,11 @@ mongoose
   .then(() => console.log("Now connected to MongoDB"))
   .catch((error) => console.log("Error connecting to MongoDB: ", error));
 
-fetchHistoricalPrices("765", "last week").then((res) => console.log(res));
+// fetchHistoricalPrices("765", "last week").then((res) => console.log(res));
 
 app.listen(port, async () => {
   // CHANGE THIS WHEN TESTING LOCALLY/ON HEROKU
-  const online = false; // if testing locally, toggle this to false; else true if deployed on heroku
+  const online = true; // if testing locally, toggle this to false; else true if deployed on heroku
   const herokuUrl = "https://phl-gs-chatbot-app.herokuapp.com";
   const webhookUrl = online ? herokuUrl : await ngrok();
   console.log("webhookUrl: ", webhookUrl);
