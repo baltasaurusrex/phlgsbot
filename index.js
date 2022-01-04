4a2e9c7085fb25ce68878c18eb78f7f685ec50ff
baltiearaneta <baltiearaneta@gmail.com>
2022-01-04 16:00:57 +0800

    fetchTimeAndSales dayjs year bug

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ const testing = async (text) => {
   }
 };
 
-testing("summary wtd");
+testing("summary 01/03");
 
 // gets called the first time a user opens the chat
 // use this as a way to register (if not already registered)
