1594af2a94d9cd98659d4b602bcc0ea006d91151
baltiearaneta <baltiearaneta@gmail.com>
2022-05-13 15:38:50 +0800

    added OHLC upload to timeAndSalesUploadCSV

--- testing.js
+++ testing.js
@@ -9,11 +9,11 @@ import {
   mapOHLCOfSecurity,
 } from "./controllers/OHLC.js";
 import { fetchTimeAndSales } from "./controllers/updates.js";
+import { uploadTimeAndSalesCSV } from "./controllers/timeAndSales.js";
+import { getTimeAndSalesCSV } from "./controllers/timeAndSales.js";
+import dayjs from "dayjs";
+import { onlyUnique } from "./utils/tools.js";
 
-const testing = async () => {
-  // getPeriod("mtd");
-  // await fetchTimeAndSales();ß
-  // await fetchHistoricalPrices("515", "weekly");
-};
+const testing = async () => {};
 
 export default testing;
--- utils/updates.js
+++ utils/updates.js
@@ -399,3 +399,5 @@ export const sortByTime = (array) => {
     return err;
   }
 };
+
+
